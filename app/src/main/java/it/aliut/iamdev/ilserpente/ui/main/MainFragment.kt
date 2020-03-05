29287275d87cf76a12fcccd1db2c42e63abae79a
a-liut <andrea.liut@gmail.com>
2020-03-05 20:11:02 +0100

    init: Basic setup.

--- app/src/main/java/it/aliut/iamdev/ilserpente/ui/main/MainFragment.kt
+++ app/src/main/java/it/aliut/iamdev/ilserpente/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package it.aliut.iamdev.ilserpente.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import it.aliut.iamdev.ilserpente.R
 
 class MainFragment : Fragment() {
