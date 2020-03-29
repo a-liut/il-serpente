72b301cdc66622ff424167da19c2e2c7aa97d4d3
a-liut <andrea.liut@gmail.com>
2020-03-29 17:33:51 +0200

    feat: extracted delay value.

--- app/src/main/java/it/aliut/iamdev/ilserpente/game/player/ComputerPlayer.kt
+++ app/src/main/java/it/aliut/iamdev/ilserpente/game/player/ComputerPlayer.kt
@@ -4,10 +4,10 @@ import it.aliut.iamdev.ilserpente.game.GameMove
 import it.aliut.iamdev.ilserpente.game.GameState
 import kotlinx.coroutines.delay
 
-class ComputerPlayer(name: String, color: Int) : Player(name, color) {
+class ComputerPlayer(name: String, color: Int, private val playDelay: Long = 500) : Player(name, color) {
 
     override suspend fun getNextMove(gameState: GameState): GameMove {
-        delay(500)
+        delay(playDelay)
         return gameState.board.allowedMoves().random()
     }
 }
